07f020e31ecefada846f2a68e0b7b2303404deae
alexanderdraev <alexanderdraev@gmail.com>
2019-06-10 12:41:49 +0300

    making some changes

---
+++ src/main/java/Report.java
@@ -0,0 +1,36 @@
+public class Report {
+
+    private int topPerformersThreshold;
+    private boolean useExprienceMultiplier;
+    private int periodLimit;
+
+    public Report(int topPerformersThreshold, boolean useExprienceMultiplier, int periodLimit) {
+        this.topPerformersThreshold = topPerformersThreshold;
+        this.useExprienceMultiplier = useExprienceMultiplier;
+        this.periodLimit = periodLimit;
+    }
+
+    public int getTopPerformersThreshold() {
+        return topPerformersThreshold;
+    }
+
+    public void setTopPerformersThreshold(int topPerformersThreshold) {
+        this.topPerformersThreshold = topPerformersThreshold;
+    }
+
+    public boolean isUseExprienceMultiplier() {
+        return useExprienceMultiplier;
+    }
+
+    public void setUseExprienceMultiplier(boolean useExprienceMultiplier) {
+        this.useExprienceMultiplier = useExprienceMultiplier;
+    }
+
+    public int getPeriodLimit() {
+        return periodLimit;
+    }
+
+    public void setPeriodLimit(int periodLimit) {
+        this.periodLimit = periodLimit;
+    }
+}
