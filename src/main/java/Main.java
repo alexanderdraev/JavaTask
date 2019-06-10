21944d3d28102d8778c0f19b0c2c78c38b2afa11
alexanderdraev <alexanderdraev@gmail.com>
2019-06-10 14:03:27 +0300

    making verification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
+import com.google.gson.Gson;
+
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        try{
+        try {
             Scanner consoleReader = new Scanner(System.in);
             String pathToDataJsonFile = consoleReader.nextLine();
             String pathToReportJsonFile = consoleReader.nextLine();
@@ -12,19 +15,36 @@ public class Main {
             Scanner dataReader = new Scanner(new FileInputStream(pathToDataJsonFile));
             String data = "";
 
-            while(dataReader.hasNextLine()){
+            while (dataReader.hasNextLine()) {
                 data += dataReader.nextLine();
             }
 
             Scanner reportReader = new Scanner(new FileInputStream(pathToReportJsonFile));
             String reportContent = "";
 
-            while (reportReader.hasNextLine()){
+            while (reportReader.hasNextLine()) {
                 reportContent += reportReader.nextLine();
+            }
+
+            Gson parser = new Gson();
+
+            Employee[] employees = parser.fromJson(data, Employee[].class);
+            Report report = parser.fromJson(reportContent, Report.class);
+
+            for (int i = 0; i < employees.length; i++) {
+                if (report.isUseExprienceMultiplier()) {
+
+                    double score = employees[i].getTotalSales() / employees[i].getSalesPeriod() * employees[i].getExperienceMultiplier();
+                    employees[i].setScore(score);
+
+                } else {
+                    double score = employees[i].getTotalSales() / employees[i].getSalesPeriod();
+                    employees[i].setScore(score);
+                }
             }
             
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
